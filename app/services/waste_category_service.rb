69030128a12b8c1ad95b4d16cee2ddfa40e6451f
urszulasologub <gold.ula4@gmail.com>
2021-04-17 20:27:02 +0200

    Calculate distance between words and keywords

--- app/services/waste_category_service.rb
+++ app/services/waste_category_service.rb
@@ -1,8 +1,32 @@
 # frozen_string_literal: true
 
 class WasteCategoryService
-  def initialize(data)
-    @code = code
+  def initialize(waste_product_hash)
+    @data = waste_product_hash
+    @keywords = [@data['name']]
+    @data['categoryDetails'].pluck('text').each do |details|
+      @keywords.append(details)
+    end
   end
 
+  def determine
+    default_category = WasteCategory.first
+    calculations = {
+      min_levenshtein: 2,
+      category: default_category
+    }
+    @keywords.each do |keyword|
+      categories = WasteCategory.all.sort do |a, b|
+        Levenshtein.distance(a.name, keyword) <=> Levenshtein.distance(b.name, keyword)
+      end
+      calc_min = Levenshtein.distance(categories.first.name, keyword)
+      next unless calc_min < calculations[:min_levenshtein]
+
+      calculations = {
+        min_levenshtein: calc_min,
+        category: categories.first
+      }
+    end
+    calculations[:category]
+  end
 end
