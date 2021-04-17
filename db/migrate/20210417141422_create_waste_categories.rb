92c33e4ea685539a14e0d2b2f194a886a818de56
urszulasologub <gold.ula4@gmail.com>
2021-04-17 16:17:14 +0200

    Add waste category migration

---
+++ db/migrate/20210417141422_create_waste_categories.rb
@@ -0,0 +1,8 @@
+class CreateWasteCategories < ActiveRecord::Migration[6.1]
+  def change
+    create_table :waste_categories do |t|
+      t.string :name, unique: true, null: false
+      t.timestamps
+    end
+  end
+end
